a5ace64c59bdd9442df5ca8b9e547756e9de4ee6
somnath khamkar <somnathkhamkar3111@gmail.com>
2026-01-03 10:42:51 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>World</h2>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </>
+    
+  )
+}
+
+export default App
---
+++ basics/script.js
@@ -0,0 +1,14 @@
+import user from './app.js';
+import {arr} from './app.js';
+
+console.log(user);
+console.log(arr);
+
+
+
+
+const h1 = document.createElement('h1');
+h1.innerHTML = `
+<h1>Hello from somnath</h1>
+`;
+document.body.appendChild(h1);
